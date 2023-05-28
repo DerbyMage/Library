39cf81e387c111190099a42f65c3d8374044cb4b
DerbyMage <133152112+DerbyMage@users.noreply.github.com>
2023-05-27 21:46:57 -0400

    boilerplate files

---
+++ library.js
@@ -0,0 +1,8 @@
+function book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+console.log(book('red fern', 'old white nigga', '222', 'cant remember')
