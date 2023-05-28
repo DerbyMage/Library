e4e8526bd2f9564b2554c231a7db8b5177c4c799
DerbyMage <133152112+DerbyMage@users.noreply.github.com>
2023-05-27 23:11:51 -0400

    Adding functions and UI

--- library.js
+++ library.js
@@ -8,4 +8,17 @@ function book(title, author, pages, read) {
 
 function addBook(){
     const library = document.querySelector('.library')
+
+    const title = document.querySelector('.title').textContent
+    const author = document.querySelector('.author').textContent
+    const pages = document.querySelector('.pages').textContent
+    const read = document.querySelector('.read').textContent
+
+    new book(title, author, pages, read)
 }
+
+function newBook() {
+    document.querySelector('.add').style.display = 'flex'
+    document.querySelector('#new-book').style.display = 'none'
+}
+
